c2ba666c12708a83b56c10c4412a23d312158ce6
FearRising <fearrising@hotmail.com>
2019-09-07 16:19:32 +0100

    Added main.cpp
    not sure if anything else is needed

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <ctime>
+#include <stdlib.h>
+#include<bits/stdc++.h>
+
+int main(){
+    std::srand(std::time(nullptr)); //generates a random seed using current time
+
+    int maxRange{100};
+    int randomIntToGuess{std::rand() % maxRange + 1};
+    int userGuess{0};
+    //std::cout << randomIntToGuess << std::endl; //debugging lol
+    std::cout << "Guess my number (1 - " << maxRange << ")\n";
+
+    while(userGuess != randomIntToGuess){
+        if(!(std::cin >> userGuess)){
+            std::cin.clear();
+            std::cin.ignore(INT_MAX, '\n');
+            std::cout << "Invalid Input\n";
+        }
+        std::cout << "Try again!\n";
+    }
+    std::cout << "\033[2J\033[1;1H You got it right!\n";
+
+    return 0;
+
+}
+
+
+    //std::cin >> userGuess;
+
+    /*
+    if (userGuess == randomIntToGuess){
+        std::cout << "YOU GUESSES IT CORRECT\n";
+    } else {
+        std::cout << "Try again\n";
+    }
+    */ //this code exits when they either get it wrong or right
+
+    /*
+    while(userGuess != randomIntToGuess){
+        std::cout << "Try again!\n";
+        std::cin >> userGuess;
+    }
+    std::cout << "You got it right!\n";
+    */
